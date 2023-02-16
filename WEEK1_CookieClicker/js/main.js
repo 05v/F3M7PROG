236234c00330bb9d5d4bda175e6e24a8e43276ef
vladverheij <vladverheij@gmail.com>
2023-02-16 14:34:51 +0100

    Les 2 Video 3

--- WEEK1_CookieClicker/js/main.js
+++ WEEK1_CookieClicker/js/main.js
@@ -12,5 +12,25 @@ class Cookie {
   }
 }
 
-const cookie = new Cookie("Default", document.getElementById("js--cookie"));
-console.log(cookie);
+class Score {
+  defaultScore = 0;
+  name = "";
+  htmlElement = undefined;
+  constructor(newDefaultScore, newName, newHTMLElement) {
+    this.defaultScore = newDefaultScore;
+    this.name = newName;
+    this.htmlElement = newHTMLElement;
+    this.htmlElement.innerText = newDefaultScore;
+  }
+}
+
+const cookie = new Cookie(
+  "Default Cookie",
+  document.getElementById("js--cookie")
+);
+const score = new Score(
+  0,
+  "Default Score",
+  document.getElementById("js--score")
+);
+console.log(cookie, score);
