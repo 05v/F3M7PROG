f8392fe70685b4d61abddcdce2e7b0034d526e00
vladverheij <vladverheij@gmail.com>
2023-02-16 15:11:24 +0100

    Les 2 Video 6

--- WEEK1_CookieClicker/js/main.js
+++ WEEK1_CookieClicker/js/main.js
@@ -30,6 +30,27 @@ class Score {
     this.score = this.score + 1;
     this.htmlElement.innerText = this.score;
   }
+
+  subtractScore() {
+    this.score = this.score - 100;
+    this.htmlElement.innerText = this.score;
+  }
+}
+
+class Multiplier {
+  factor = 1;
+  htmlElement = undefined;
+  cookie = undefined;
+  constructor(newHTMLElement, cookie) {
+    this.htmlElement = newHTMLElement;
+    this.cookie = cookie;
+    this.htmlElement.onclick = this.onMultiplierClick;
+  }
+
+  onMultiplierClick = () => {
+    console.log("Multiplier button clicked");
+    this.cookie.score.subtractScore();
+  };
 }
 
 const score = new Score(
@@ -43,3 +64,9 @@ const cookie = new Cookie(
   document.getElementById("js--cookie"),
   score
 );
+
+const multiplier = new Multiplier(
+  document.getElementById("js--multiplier"),
+  cookie
+);
+console.log(multiplier);
