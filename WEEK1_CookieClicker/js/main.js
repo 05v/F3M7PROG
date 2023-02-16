4d3a6611d6a1d4fce67dd37690c28fcbf53ea353
vladverheij <vladverheij@gmail.com>
2023-02-16 14:28:57 +0100

    Les 2 Video 2

--- WEEK1_CookieClicker/js/main.js
+++ WEEK1_CookieClicker/js/main.js
@@ -1,9 +1,16 @@
 class Cookie {
-  name = "";
-  constructor(newName) {
-    this.name = newName;
+  cookieType = "";
+  htmlElement = undefined;
+  constructor(newCookieType, newHTMLElement) {
+    this.cookieType = newCookieType;
+    this.htmlElement = newHTMLElement;
+    this.htmlElement.onclick = this.onCookieClick;
+  }
+
+  onCookieClick() {
+    console.log("Clicked");
   }
 }
 
-const test = new Cookie("");
-console.log(test.name);
+const cookie = new Cookie("Default", document.getElementById("js--cookie"));
+console.log(cookie);
