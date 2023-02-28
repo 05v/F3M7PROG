e6ea630aae5325056a60b4d21d6c63cfabf1bda7
vladverheij <vladverheij@gmail.com>
2023-02-28 15:48:08 +0100

    Les 3 Video 13

--- WEEK2_ColorPicker/js/main.js
+++ WEEK2_ColorPicker/js/main.js
@@ -2,18 +2,18 @@ const colors = document.getElementsByClassName("colors__color");
 
 for (let i = 0; i < colors.length; i++) {
   // Number 1-360, the Hue
-  let randomHue = Math.random * (360 - 1) + 1;
+  let randomHue = Math.floor(Math.random() * (360 - 1) + 1);
 
   // Percentage 11-79, the Saturation
-  let randomSaturation = Math.random * (79 - 11) + 11;
+  let randomSaturation = Math.floor(Math.random() * (79 - 11) + 11) + "%";
 
   // Percentage 11-100, the Brightness
-  let randomBrightness = Math.random * (100 - 11) + 11;
+  let randomBrightness = Math.floor(Math.random() * (100 - 11) + 11) + "%";
 
   // Set color
-  colors[i].children[0].style.background = `hsl(${
-    (randomHue, randomSaturation, randomBrightness)
-  })`;
+  colors[
+    i
+  ].children[0].style.background = `hsl(${randomHue} ${randomSaturation} ${randomBrightness})`;
 
   // On click
   colors[i].onclick = function () {
