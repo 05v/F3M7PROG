7aa4dd86f365ffe7b35eb0293489b3e0ed2182c4
vladverheij <vladverheij@gmail.com>
2023-02-28 15:28:01 +0100

    Les 3 Video 12

--- WEEK2_ColorPicker/js/main.js
+++ WEEK2_ColorPicker/js/main.js
@@ -1,15 +1,19 @@
 const colors = document.getElementsByClassName("colors__color");
-let secondColor = false;
 
 for (let i = 0; i < colors.length; i++) {
+  // Number 1-360, the Hue
+  let randomHue = Math.random * (360 - 1) + 1;
+
+  // Percentage 11-79, the Saturation
+  let randomSaturation = Math.random * (79 - 11) + 11;
+
+  // Percentage 11-100, the Brightness
+  let randomBrightness = Math.random * (100 - 11) + 11;
+
   // Set color
-  if (secondColor === false) {
-    colors[i].children[0].style.background = "#635985";
-    secondColor = true;
-  } else {
-    colors[i].children[0].style.background = "#F48484";
-    secondColor = false;
-  }
+  colors[i].children[0].style.background = `hsl(${
+    (randomHue, randomSaturation, randomBrightness)
+  })`;
 
   // On click
   colors[i].onclick = function () {
