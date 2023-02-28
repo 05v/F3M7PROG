e506103afbe7bc47e54a708c46dc3369983a454f
vladverheij <vladverheij@gmail.com>
2023-02-28 15:03:44 +0100

    Les 3 Video 10

--- WEEK2_ColorPicker/js/main.js
+++ WEEK2_ColorPicker/js/main.js
@@ -1,7 +1,13 @@
 const colors = document.getElementsByClassName("colors__color");
 
 for (let i = 0; i < colors.length; i++) {
+  // Set color
+  colors[i].children[0].style.background = "#635985";
+
+  // On click
   colors[i].onclick = function () {
     colors[i].children[0].classList.add("colors__circle--selected");
+    navigator.clipboard.writeText(colors[i].children[0].style.background);
+    document.title = colors[i].children[0].style.background;
   };
 }
