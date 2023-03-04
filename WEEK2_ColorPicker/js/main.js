58f9fc352f0da0643b7f2cbc87aa6a48250f4e46
vladverheij <vladverheij@gmail.com>
2023-03-04 15:19:08 +0100

    Les 4 Video 1

--- WEEK2_ColorPicker/js/main.js
+++ WEEK2_ColorPicker/js/main.js
@@ -1,24 +1,14 @@
-const colors = document.getElementsByClassName("colors__color");
-
-for (let i = 0; i < colors.length; i++) {
-  // Number 1-360, the Hue
-  let randomHue = Math.floor(Math.random() * (360 - 1) + 1);
-
-  // Percentage 11-79, the Saturation
-  let randomSaturation = Math.floor(Math.random() * (79 - 11) + 11) + "%";
-
-  // Percentage 11-100, the Brightness
-  let randomBrightness = Math.floor(Math.random() * (100 - 11) + 11) + "%";
-
-  // Set color
-  colors[
-    i
-  ].children[0].style.background = `hsl(${randomHue} ${randomSaturation} ${randomBrightness})`;
-
-  // On click
-  colors[i].onclick = function () {
-    colors[i].children[0].classList.add("colors__circle--selected");
-    navigator.clipboard.writeText(colors[i].children[0].style.background);
-    document.title = colors[i].children[0].style.background;
-  };
+class ColorCard {
+  id;
+  color;
+  constructor(newId, newColor) {
+    this.id = newId;
+    this.color = newColor;
+  }
 }
+
+const test = new ColorCard(101, "rgba(0,0,0,0)");
+console.log(test);
+
+const test2 = new ColorCard(3002, "red");
+console.log(test2);
---
+++ WEEK2_ColorPicker/js/script-main.js
@@ -0,0 +1,24 @@
+const colors = document.getElementsByClassName("colors__color");
+
+for (let i = 0; i < colors.length; i++) {
+  // Number 1-360, the Hue
+  let randomHue = Math.floor(Math.random() * (360 - 1) + 1);
+
+  // Percentage 11-79, the Saturation
+  let randomSaturation = Math.floor(Math.random() * (79 - 11) + 11) + "%";
+
+  // Percentage 11-100, the Brightness
+  let randomBrightness = Math.floor(Math.random() * (100 - 11) + 11) + "%";
+
+  // Set color
+  colors[
+    i
+  ].children[0].style.background = `hsl(${randomHue} ${randomSaturation} ${randomBrightness})`;
+
+  // On click
+  colors[i].onclick = function () {
+    colors[i].children[0].classList.add("colors__circle--selected");
+    navigator.clipboard.writeText(colors[i].children[0].style.background);
+    document.title = colors[i].children[0].style.background;
+  };
+}
