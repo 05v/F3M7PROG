0268396f20f039cfefd266cab38cbeb339f136e9
vladverheij <vladverheij@gmail.com>
2023-03-04 15:52:45 +0100

    Les 4 Video 4

--- WEEK2_ColorPicker/js/main.js
+++ WEEK2_ColorPicker/js/main.js
@@ -2,26 +2,40 @@ class ColorCard {
   id;
   color;
   constructor(newId, newColor, addToList) {
+    // Setting properties
     this.id = newId;
     this.color = newColor;
     this.addToList = addToList;
-  }
 
-  render() {
-    const listItemToBeRendered = document.createElement("li");
-    listItemToBeRendered.classList = "colors__color";
+    // Creates <li> with class 'colors__color'
+    this.htmlElement = document.createElement("li");
+    this.htmlElement.classList = "colors__color";
 
+    // Creates <figure> with class 'colors__circle' and adds it to the <li>
     const figureToBeRendered = document.createElement("figure");
     figureToBeRendered.classList = "colors__circle";
     figureToBeRendered.style.background = this.color;
+    this.htmlElement.appendChild(figureToBeRendered);
 
+    // Creates <p> with class 'colors__text' with the text Copied in it and adds it to the <li>
     const pToBeRendered = document.createElement("p");
     pToBeRendered.classList = "colors__text";
     pToBeRendered.innerText = "Copied";
+    this.htmlElement.appendChild(pToBeRendered);
 
-    listItemToBeRendered.appendChild(figureToBeRendered);
-    listItemToBeRendered.appendChild(pToBeRendered);
-    this.addToList.appendChild(listItemToBeRendered);
+    //
+    this.htmlElement.onclick = this.onHTMLElementClicked();
+
+    this.render();
+  }
+
+  onHTMLElementClicked = function () {
+    console.log("I clicked" + this.color);
+  };
+
+  render() {
+    // Adds the <li> with all it's children elements into the <ul>
+    this.addToList.appendChild(this.htmlElement);
   }
 }
 
@@ -30,6 +44,3 @@ const test = new ColorCard(
   "rgba(255,0,0,1)",
   document.getElementById("js--colors")
 );
-
-test.render();
-console.log(test);
