59a0d392b747a617cdb1951d2aa4c0f0fdc409bf
vladverheij <vladverheij@gmail.com>
2023-03-04 15:35:19 +0100

    Les 4 Video 2

--- WEEK2_ColorPicker/js/main.js
+++ WEEK2_ColorPicker/js/main.js
@@ -1,14 +1,34 @@
 class ColorCard {
   id;
   color;
-  constructor(newId, newColor) {
+  constructor(newId, newColor, addToList) {
     this.id = newId;
     this.color = newColor;
+    this.addToList = addToList;
+  }
+
+  render() {
+    const listItemToBeRendered = document.createElement("li");
+    listItemToBeRendered.classList = "colors__color";
+
+    const figureToBeRendered = document.createElement("figure");
+    figureToBeRendered.classList = "colors__circle";
+
+    const pToBeRendered = document.createElement("p");
+    pToBeRendered.classList = "colors__text";
+    pToBeRendered.innerText = "Copied";
+
+    listItemToBeRendered.appendChild(figureToBeRendered);
+    listItemToBeRendered.appendChild(pToBeRendered);
+    this.addToList.appendChild(listItemToBeRendered);
   }
 }
 
-const test = new ColorCard(101, "rgba(0,0,0,0)");
-console.log(test);
+const test = new ColorCard(
+  101,
+  "rgba(0,0,0,0)",
+  document.getElementById("js--colors")
+);
 
-const test2 = new ColorCard(3002, "red");
-console.log(test2);
+test.render();
+console.log(test);
