23790f8559f8ca0d5d52546cf22a371e4965228a
vladverheij <vladverheij@gmail.com>
2023-03-21 09:37:22 +0100

    Les 8 Video 2

--- WEEK4_Banky/js/main.js
+++ WEEK4_Banky/js/main.js
@@ -1,7 +1,22 @@
-const transactions = fetch("/data/transactions.json")
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    console.log(data);
-  });
+class GetDataFromApi {
+  url = "";
+  data = null;
+
+  constructor(newURL) {
+    this.url = newURL;
+  }
+
+  async getData() {
+    await fetch(this.url)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        this.data = data;
+      });
+  }
+}
+
+const jeroen = new GetDataFromApi("/data/transactions.json");
+jeroen.getData();
+console.log(jeroen.data);
