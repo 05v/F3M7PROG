c1263434b311880c8da26da6fdba7437590cd62b
vladverheij <vladverheij@gmail.com>
2023-04-16 15:02:42 +0200

    Les 10 Video 3

--- WEEK5_GamesBacklog/js/main.js
+++ WEEK5_GamesBacklog/js/main.js
@@ -1,1 +1,34 @@
-console.log("Link");
+class Api {
+  data = null;
+  async getData() {
+    await fetch("../data/games.json")
+      .then((response) => {
+        return response.json();
+      })
+      .then((newData) => {
+        this.data = newData.games; //newData["games"]
+      });
+  }
+}
+
+class Filter {
+  filter(platform) {
+    console.log(platform);
+  }
+}
+
+class App {
+  api;
+  filter;
+  constructor() {
+    this.api = new Api();
+    this.filter = new Filter();
+
+    this.api.getData().then(() => {
+      console.log(this.api.data);
+    });
+    this.filter.filter("PS4");
+  }
+}
+
+const app = new App();
